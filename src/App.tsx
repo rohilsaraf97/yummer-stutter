3567031aa5eabc1ab8dd7449c76934e6884c4846
Rohil Saraf <rohilsaraf97@gmail.com>
2022-08-11 20:50:10 +0530

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import RecipeForm from "./components/Recipe/RecipeForm";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
-function App() {
+const App = () => {
+  const checkIfAuth = localStorage.getItem("isAuth") ? true : false;
+  const [isAuth, setIsAuth] = useState(checkIfAuth);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar auth={isAuth} setAuth={setIsAuth} />
+      <Routes>
+        <Route path="/recipes" element={<Home />} />
+        <Route
+          path="/login"
+          element={
+            isAuth ? <Navigate to="/recipes" /> : <Login setAuth={setIsAuth} />
+          }
+        />
+        <Route path="*" element={<Navigate to="/recipes" replace />} />
+        <Route path="/recipes/new" element={<RecipeForm />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
